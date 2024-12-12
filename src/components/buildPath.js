c7767d89701b42dca0da8d52619f24a656ad1d48
Shane Welz <ShaneWelz@gmail.com>
2024-12-12 16:07:32 -0500

    fixed buildpath issue

--- src/components/buildPath.js
+++ src/components/buildPath.js
@@ -1,6 +1,6 @@
 export function buildPath(route) {
   if (process.env.NODE_ENV === 'production') {
-    return 'https://206.81.1.248/' + route;
+    return 'http://206.81.1.248/' + route;
   } else {
     return 'http://localhost:5000/' + route;
   }
