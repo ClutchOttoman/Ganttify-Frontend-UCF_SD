87c21193d9a406dbd6b55151a4638137e5a9963a
Connor34Bennett <co617187@ucf.edu>
2025-01-20 15:04:29 -0500

    Main Work for Adjusting Time Table

--- src/components/GanttChart/GanttChart.js
+++ src/components/GanttChart/GanttChart.js
@@ -26,6 +26,7 @@ export default function GanttChart({ projectId, setUserRole, userRole }) {
     fromSelectYear: currentYear.toString(),
     toSelectMonth: currentMonth + 1, 
     toSelectYear: currentYear.toString(),
+    selectedRange: "",
   });
 
   const [selectedTask, setSelectedTask] = useState(null);
@@ -145,9 +146,23 @@ export default function GanttChart({ projectId, setUserRole, userRole }) {
   }, [tasks]);
 
   const handleTimeRangeChange = (event) => {
-    const selectedRange = event.target.value;
+    let curMonth = new Date().getMonth();
+    let curYear = new Date().getFullYear();
+    let selectedRange = event.target.value;
+
+    let updatedTimeRange = {
+      fromSelectMonth: curMonth,
+      fromSelectYear: curYear.toString(),
+      toSelectMonth: (curMonth + 1) % 12,
+      toSelectYear:
+      curMonth === 11
+          ? (curYear + 1).toString()
+          : curYear.toString(),
+      selectedRange: selectedRange,
+    };
+
     console.log("Selected Time Range:", selectedRange);
-    setTimeRange(selectedRange); // Update the timeRange state to trigger TimeTable re-render
+    setTimeRange(updatedTimeRange); // Update the timeRange state to trigger TimeTable re-render
   };
   
 
