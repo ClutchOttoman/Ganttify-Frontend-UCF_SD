9f96b2147bdc7e39608d29bf9a185a1d8a8fd300
ShadowBoxing7 <ashkoltz91@gmail.com>
2025-03-01 00:19:24 -0500

    Update Part One For About Us
    For better maintenance for future teams.

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -8,103 +8,123 @@ import member_kr from '../Images/assets/team_member_icons/KR.jpg';
 import member_sw from '../Images/assets/team_member_icons/SW.jpg';
 import member_oc from '../Images/assets/team_member_icons/OC.jpg';
 import member_ak from '../Images/assets/team_member_icons/AK.PNG';
+import member_placeholder from '../Images/assets/team_member_icons/Placeholder.jpg';
 import {buildPath} from './buildPath';
 
 
 function AboutUs() {
 
+    return (
 
-  return (
-   
-<div>
+        <div style={{alignContent: "center"}}>
 
-    <br></br>
-    <br></br>
-    <br></br>
+            {/* SD1 and SD2 Credits. */}
+            <div className='dropdown-section'>
+                <button style={{backgroundColor: "#DC6B2C", borderRadius: "8px", border: "none"}}>
+                    Senior Design Fall and Spring 2025 Team
+                </button>
 
-    <div className="bottomDiv">
-
-        <div className = "box">
-        
-
-        <div className="team-member">
-            <img src={member_nb} alt="Nickolas Brandenburg" className="team-image"/>   
-            <div className="member-info">
-                    <h2>Nickolas Brandenburg</h2>
-                    <p>Database and API developer</p>
-            </div>
-        </div>
-
-        <div className="team-member-reverse">
-            <div className="member-info">
-                <div className="member-text-info-reverse">
-                    <h2>Ash Koltz</h2>
-                    <p>Project Manager and Artist</p>
+                <div className="team-member-alt">
+                    <img src={member_placeholder} alt="Ash Koltz" className="team-image"/>
+                    <div className="member-info">
+                        <h2>Connor Bennett</h2>
+                        <p>Frontend</p>
+                    </div>
                 </div>
-            </div>
-            <img src={member_ak} alt="Ash Koltz" className="team-image"/>
-        </div>
 
-        <div className="team-member">
-            <img src={member_tk} alt="Tetiana Kotvitska" className="team-image"/>
-            <div className="member-info">
-                <h2>Tetiana Kotvitska</h2>
-                <p>Database and API developer</p>
-            </div>
-        </div>
-
-        <div className="team-member-reverse">
-            <div className="member-info">
-                <div className="member-text-info-reverse">
-                    <h2>Aaron Nogues</h2>
-                    <p>Mobile app developer</p>
+                <div className="team-member">
+                    <img src={member_ak} alt="Ash Koltz" className="team-image"/>
+                    <div className="member-info">
+                        <h2>Ash Koltz</h2>
+                        <p>Project Manager and Artist</p>
+                    </div>
                 </div>
-            </div>
-            <img src={member_an} alt="Aaron Nogues" className="team-image"/>
-        </div>
 
-        <div className="team-member">
-            <img src={member_kr} alt="Keyliz Rodriguez" className="team-image"/>
-            <div className="member-info">
-                <h2>Keyliz Rodriguez</h2>
-                <p>Frontend developer</p>
-            </div>
-        </div>
-
-        <div className="team-member-reverse">
-            <div className="member-info">
-                <div className="member-text-info-reverse">
-                    <h2>Luis Rodriguez-Rivera</h2>
-                    <p>Database and API developer</p>
+                <div className="team-member-alt">
+                    <img src={member_tk} alt="Tetiana Kotvitska" className="team-image"/>
+                    <div className="member-info">
+                        <h2>Tetiana Kotvitska</h2>
+                        <p>Database and API developer</p>
+                    </div>
                 </div>
-            </div>
-            <img src={member_lr} alt="Luis Rodriguez-Rivera" className="team-image"/>
-        </div>
 
-        <div className="team-member">
-            <img src={member_oc} alt="Omar Castro" className="team-image"/>
-            <div className="member-info">
-                <h2>Omar Castro</h2>
-                <p>Frontend developer</p>
-            </div>
-        </div>
-
-        <div className="team-member-reverse">
-            <div className="member-info">
-                <div className="member-text-info-reverse">
-                    <h2>Shane Welz</h2>
-                    <p>Frontend and API developer</p>
+                <div className="team-member">
+                    <img src={member_placeholder} alt="Ash Koltz" className="team-image"/>
+                    <div className="member-info">
+                        <h2>Allen Menorca</h2>
+                        <p>Project Manager and Frontend</p>
+                    </div>
                 </div>
+
+                <div className="team-member-alt">
+                    <img src={member_placeholder} alt="Ash Koltz" className="team-image"/>
+                    <div className="member-info">
+                        <h2>Jose Neto</h2>
+                        <p>Frontend</p>
+                    </div>
+                </div>
+                
+                <div className="team-member">
+                    <img src={member_sw} alt="Shane Welz" className="team-image"/>
+                    <div className="member-info">
+                        <h2>Shane Welz</h2>
+                        <p>Frontend and API developer</p>
+                    </div>
+                </div>
+
             </div>
-            <img src={member_sw} alt="Shane Welz" className="team-image"/>
+
+            {/* From COP 4331 Semester */}
+            <div className='dropdown-section'>
+                <button style={{backgroundColor: "#DC6B2C", borderRadius: "8px", border: "none"}}>
+                    COP 4331 Summer 2024 Team
+                </button>
+
+                <div className='dropdown-content'>
+                    {/* Team members. */}
+
+                    <div className="team-member-alt">
+                        <img src={member_nb} alt="Nickolas Brandenburg" className="team-image"/>
+                        <div className="member-info">
+                            <h2>Nickolas Brandenburg</h2>
+                            <p>Database and API developer</p>
+                        </div> 
+                    </div>
+
+                    <div className="team-member">
+                        <img src={member_oc} alt="Omar Castro" className="team-image"/>
+                        <div className="member-info">
+                            <h2>Omar Castro</h2>
+                            <p>Frontend developer</p>
+                        </div>
+                    </div> 
+
+                    <div className="team-member-alt">
+                        <img src={member_an} alt="Aaron Nogues" className="team-image"/>
+                        <div className="member-info">
+                            <div className="member-info">
+                                <h2>Aaron Nogues</h2>
+                                <p>Mobile app developer</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="team-member">
+                        <img src={member_lr} alt="Luis Rodriguez-Rivera" className="team-image"/>
+                        <div className="member-info">
+                            <div className="member-text-info-reverse">
+                                <h2>Luis Rodriguez-Rivera</h2>
+                                <p>Database and API developer</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                
+            </div>
         </div>
-        </div>
-    </div>
-</div>
 
-
-
-  );
+    );
 };
 
 export default AboutUs;
