e404effaf1789bdf88c0dc0406032740c9d6cf90
Almenorca <105606096+Almenorca@users.noreply.github.com>
2025-03-10 14:44:55 -0400

    Update AnnouncementModal.js
    Adding maintenance msg

--- src/components/AnnouncementModal.js
+++ src/components/AnnouncementModal.js
@@ -27,15 +27,9 @@ function AnnouncementModal() {
                             </div>
                             <div className="modal-body">
                                 {/* Write new features or updates here */}
-                                <h3>New Features</h3>
-                                <p>- New and Improved todo list.</p>
-                                <p>- Updated About Us Section to feature this semester's Ganttify team!</p>
-                                <h3>Bug Fixes</h3>
-                                <p>- Fixing timeline rendering when updating task start/end dates and categories.</p>         
-                                <p>- Fixed various visual bugs.</p>
-                                <h3>Next Update</h3>
-                                <p>- More customization options for UI and other new features.</p>
-                                <p>Thank you for the feedback. Continue to let us know about any bugs and potential features you would like to see in the future!</p>                                   
+                                <h3>Server Maintenance</h3>
+                                <p>Dear Users,<br></br><br></br>
+Please be advised that our server will undergo scheduled maintenance on 3/14 at 11:00 AM. During this time, the website will be temporarily unavailable. We apologize for any inconvenience this may cause and appreciate your understanding.</p>                                 
                             </div>
                             <div className="modal-footer">
                                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={handleCloseModal}>Close</button>
